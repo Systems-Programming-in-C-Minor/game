7e36577ad1d4d35abd26a38fa216d6b1baf4bee1
mrcotrmpr <marco1088@hotmail.nl>
2022-12-05 11:19:06 +0100

    CU-377pvub - Create SpeedIndicator

--- include/car.hpp
+++ include/car.hpp
@@ -9,8 +9,11 @@ public:
     Car(const std::string& name, const std::string& tag, const std::string sprite_path);
     [[nodiscard]] std::string get_sprite_path() const;
     void set_sprite_path(std::string sprite_path);
+    [[nodiscard]] float get_current_speed() const;
+    void set_current_speed(float speed);
 private:
     std::string _sprite_path;
+    float _current_speed;
 };
 
 #endif //GAME_CAR_HPP
--- src/car.cpp
+++ src/car.cpp
@@ -10,3 +10,11 @@ std::string Car::get_sprite_path() const {
 void Car::set_sprite_path(std::string sprite_path) {
     _sprite_path = sprite_path;
 }
+
+float Car::get_current_speed() const {
+    return _current_speed;
+}
+
+void Car::set_current_speed(float speed) {
+    _current_speed = speed;
+}
