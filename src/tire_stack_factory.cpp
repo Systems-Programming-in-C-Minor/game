de4c5e7c7aa29b0e850b6a02c9fa253a8a1b555c
marco_rivia <marco1088@hotmail.nl>
2022-12-08 12:26:02 +0100

    add transform and sprite

--- src/tire_stack_factory.cpp
+++ src/tire_stack_factory.cpp
@@ -1,11 +1,11 @@
 #include "tire_stack_factory.hpp"
 
 #include "transform.hpp"
+#include "components/sprite.hpp"
 
-GameObject TireStackFactory::get(const std::string &name, const std::string &tag, Vector2d &pos, double scale) {
-    GameObject obj (name, tag);
-    Transform transform(pos, 0, scale);
-    // TODO: Add transform to GameObject
-
+GameObject TireStackFactory::get(const std::string &name, const std::string &tag, Vector2d &pos, float scale) {
+    GameObject obj (name, tag, true, Transform { pos, 0.0f, scale});
+    std::shared_ptr<Sprite> sprite = std::make_shared<Sprite>("../assets/Barriers/tire_stack.png", Color(0,0,0,0), false, false, 1, 1);
+    obj.add_component(sprite);
     return obj;
 }
