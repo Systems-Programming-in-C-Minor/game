9b58c67436d44c4d88693bc62121f7804e31de92
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2022-12-10 19:59:20 +0100

    Add angle and scale to key binds

--- src/car/car.cpp
+++ src/car/car.cpp
@@ -1,15 +1,18 @@
 #include "car/car.hpp"
 #include "components/sprite.hpp"
 #include "color.hpp"
+#include "utils/trigonometry.hpp"
 
 Car::Car(const std::string &name, const std::string &tag, std::string sprite_path) :
         GameObject(name, tag) {
     transform.set_scale(0.04f);
-    std::shared_ptr<Sprite> sprite = std::make_shared<Sprite>(std::move(sprite_path), Color(0, 0, 0, 0), false, false,
-                                                              1, 1);
+    transform.set_angle(degrees_to_radians(180));
+    auto sprite =
+            std::make_shared<Sprite>(std::move(sprite_path), Color(0, 0, 0, 0), false, false, 1, 1);
+
     add_component(sprite);
 }
-    
+
 int Car::get_current_round() const {
     return _current_round;
 }
--- src/car/player_car_behaviour.cpp
+++ src/car/player_car_behaviour.cpp
@@ -1,9 +1,36 @@
 #include "car/player_car_behaviour.hpp"
+#include <gameobject.hpp>
 
 PlayerCarBehaviour::PlayerCarBehaviour(EventManager &event_manager) : KeyListener(event_manager) {}
 
-void PlayerCarBehaviour::on_key_pressed(const KeyPressedEvent &event) {}
+void PlayerCarBehaviour::on_key_pressed(const KeyPressedEvent &event) {
+    switch (event.key) {
+        case EQUAL:
+            game_object->transform.set_scale(game_object->transform.get_scale() + 0.01f);
+            break;
+        case MIN:
+            game_object->transform.set_scale(game_object->transform.get_scale() - 0.01f);
+            break;
+        case Z:
+            game_object->transform.set_angle(game_object->transform.get_angle() + 0.1f);
+            break;
+        case X:
+            game_object->transform.set_angle(game_object->transform.get_angle() - 0.1f);
+            break;
+    }
+}
 
-void PlayerCarBehaviour::on_key_hold(const KeyHoldEvent &event) {}
+void PlayerCarBehaviour::on_key_hold(const KeyHoldEvent &event) {
+    switch (event.key) {
+        case W:
+            break;
+        case S:
+            break;
+        case A:
+            break;
+        case D:
+            break;
+    }
+}
 
 void PlayerCarBehaviour::on_key_released(const KeyReleasedEvent &event) {}
