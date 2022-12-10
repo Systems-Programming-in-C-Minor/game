9b3a58efbdb66a6849a9e6898749cdf8839a2620
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2022-12-10 17:28:07 +0100

    Fix tire stack factory

--- src/tire_stack_factory.cpp
+++ src/tire_stack_factory.cpp
@@ -1,11 +1,13 @@
 #include "tire_stack_factory.hpp"
-
 #include "transform.hpp"
 #include "components/sprite.hpp"
 
-GameObject TireStackFactory::get(const std::string &name, const std::string &tag, Vector2d &pos, float scale) {
-    GameObject obj (name, tag, true, Transform { pos, 0.0f, scale});
-    std::shared_ptr<Sprite> sprite = std::make_shared<Sprite>("../assets/Barriers/tire_stack.png", Color(0,0,0,0), false, false, 1, 1);
-    obj.add_component(sprite);
+std::shared_ptr<GameObject>
+TireStackFactory::get(const std::string &name, const std::string &tag, Vector2d &pos, float scale) {
+    std::shared_ptr<GameObject> obj =
+            std::make_shared<GameObject>(name, tag, true, Transform{pos, 0.0f, scale});
+    std::shared_ptr<Sprite> sprite =
+            std::make_shared<Sprite>("../assets/barriers/tire_stack.png", Color(0, 0, 0, 0), false, false, 1, 1);
+    obj->add_component(sprite);
     return obj;
 }
