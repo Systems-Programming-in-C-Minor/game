a3a4861c6aec0527c8d1c358df5b649dddeb7c05
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2022-12-11 19:10:48 +0100

    Implement top down physics

--- include/car/player_car_behaviour.hpp
+++ include/car/player_car_behaviour.hpp
@@ -6,7 +6,42 @@
 #include <listeners/key_listener.hpp>
 #include "interfaces/itickable.hpp"
 
+enum DriveState {
+    NeutralDrive,
+    Forward,
+    Backward,
+};
+
+
+enum SteerState {
+    NeutralSteer,
+    Right,
+    Left
+};
+
 class PlayerCarBehaviour : public Component, public KeyListener, public ITickable {
+
+private:
+    const float max_speed_forward = 1.f;
+    const float max_speed_backwards = -1.f;
+    const float max_drive_force = 1.f;
+    const float current_traction = 1.f;
+    const float steer_torque = 5.f;
+    const float steer_torque_offset = 2.f;
+    const float max_lateral_impulse = 1.f;
+    const float drift_friction = 1.f;
+    const float angular_friction = 1.f;
+    const float drag_modifier = 1.f;
+
+    DriveState drive_state = NeutralDrive;
+    SteerState steer_state = NeutralSteer;
+
+    void friction();
+
+    void drive();
+
+    void turn();
+
 public:
     explicit PlayerCarBehaviour(EventManager &event_manager);
 
--- src/car/player_car_behaviour.cpp
+++ src/car/player_car_behaviour.cpp
@@ -6,51 +6,104 @@
 PlayerCarBehaviour::PlayerCarBehaviour(EventManager &event_manager) : KeyListener(event_manager) {}
 
 void PlayerCarBehaviour::tick(GameObject &object) {
+    friction();
+    drive();
+    turn();
+}
 
+void PlayerCarBehaviour::friction() {
+    auto &body = *game_object->get_component<RigidBody>();
+
+
+    //calculate the counter lateral impulse based on drift parameters
+    Vector2d impulse = Vector2d{body.get_mass() * -body.get_lateral_velocity().x,
+                                body.get_mass() * -body.get_lateral_velocity().y};
+    if (impulse.length() > max_lateral_impulse)
+        impulse = Vector2d{impulse.x * max_lateral_impulse / impulse.length(),
+                           impulse.y * max_lateral_impulse / impulse.length()};
+
+    // apply the impulse
+    body.apply_linear_impulse(Vector2d{impulse.x * drift_friction, impulse.y * drift_friction},
+                              body.get_world_center());
+
+    //angular velocity
+    body.apply_angular_impulse(angular_friction * body.get_inertia() * -body.get_angular_velocity());
+
+    //forward linear velocity
+    Vector2d currentForwardNormal = body.get_forward_velocity();
+    float currentForwardSpeed = currentForwardNormal.normalize();
+    float dragForceMagnitude = -2 * currentForwardSpeed * drag_modifier;
+
+    body.apply_force(Vector2d{currentForwardNormal.x * current_traction * dragForceMagnitude,
+                              currentForwardNormal.y * current_traction * dragForceMagnitude}, body.get_world_center());
+}
+
+void PlayerCarBehaviour::drive() {
+    auto &body = *game_object->get_component<RigidBody>();
+
+    //find current speed in forward direction
+    float current_speed = body.get_current_speed();
+
+    float desired_speed = 0.f;
+    if (drive_state == Forward)
+        desired_speed = max_speed_forward;
+    if (drive_state == Backward)
+        desired_speed = max_speed_backwards;
+
+    //apply necessary force
+    float force = (desired_speed > current_speed) ? max_drive_force : -max_drive_force;
+    if (desired_speed != current_speed) {
+        auto force_vec = Vector2d{body.get_world_vector(Vector2d{0, 1}).x * current_traction * force,
+                                  body.get_world_vector(Vector2d{0, 1}).y * current_traction * force};
+        body.apply_force(force_vec, body.get_world_center());
+    }
+}
+
+void PlayerCarBehaviour::turn() {
+    float desiredTorque = 0;
+    // set our torque
+    float torque = steer_torque + steer_torque_offset;
+    if (steer_state == Left)
+        desiredTorque = torque;
+    if (steer_state == Right)
+        desiredTorque = -torque;
+
+    // reverse the torque if we are going backwards
+    if (drive_state == Backward)
+        desiredTorque = -desiredTorque;
+
+    game_object->get_component<RigidBody>()->apply_torque(desiredTorque);
 }
 
 void PlayerCarBehaviour::on_key_pressed(const KeyPressedEvent &event) {
     switch (event.key) {
+        case W: {
+            drive_state = Forward;
+            break;
+        }
+        case S: {
+            drive_state = Backward;
+            break;
+        }
+        case A: {
+            steer_state = Left;
+            break;
+        }
+        case D: {
+            steer_state = Right;
+            break;
+        }
         case EQUAL:
             game_object->transform.set_scale(game_object->transform.get_scale() + 0.01f);
             break;
         case MIN:
             game_object->transform.set_scale(game_object->transform.get_scale() - 0.01f);
             break;
-        case Z:
-            game_object->transform.set_angle(game_object->transform.get_angle() + 0.1f);
-            break;
-        case X:
-            game_object->transform.set_angle(game_object->transform.get_angle() - 0.1f);
-            break;
     }
 }
 
 void PlayerCarBehaviour::on_key_hold(const KeyHoldEvent &event) {
-    auto center = game_object->transform.get_position();
-    auto angle = game_object->transform.get_angle();
-    auto angle_force = Vector2d{(float) sin(angle), (float) cos(angle)};
-
     switch (event.key) {
-        case W: {
-            game_object->get_component<RigidBody>()->apply_force(angle_force, center);
-            break;
-        }
-        case S: {
-            angle_force = Vector2d{angle_force.x * -1.f, angle_force.y * -1.f};
-            game_object->get_component<RigidBody>()->apply_force(angle_force, center);
-            break;
-        }
-        case A: {
-            game_object->get_component<RigidBody>()->apply_torque(1.2f);
-//            game_object->get_component<RigidBody>()->apply_force(angle_changed);
-            break;
-        }
-        case D: {
-            game_object->get_component<RigidBody>()->apply_torque(-1.2f);
-//            game_object->get_component<RigidBody>()->set_linear_velocity(angle_changed);
-            break;
-        }
         case UP: {
             Vector2d pos_up{game_object->transform.get_position().x,
                             game_object->transform.get_position().y + 0.01f};
@@ -75,7 +128,36 @@ void PlayerCarBehaviour::on_key_hold(const KeyHoldEvent &event) {
             game_object->transform.set_position(pos_right);
             break;
         }
+        case Z:
+            game_object->transform.set_angle(game_object->transform.get_angle() + 0.01f);
+            break;
+        case X:
+            game_object->transform.set_angle(game_object->transform.get_angle() - 0.01f);
+            break;
     }
 }
 
-void PlayerCarBehaviour::on_key_released(const KeyReleasedEvent &event) {}
+void PlayerCarBehaviour::on_key_released(const KeyReleasedEvent &event) {
+    switch (event.key) {
+        case W: {
+            if (drive_state == Forward)
+                drive_state = NeutralDrive;
+            break;
+        }
+        case S: {
+            if (drive_state == Backward)
+                drive_state = NeutralDrive;
+            break;
+        }
+        case A: {
+            if (steer_state == Left)
+                steer_state = NeutralSteer;
+            break;
+        }
+        case D: {
+            if (steer_state == Right)
+                steer_state = NeutralSteer;
+            break;
+        }
+    }
+}
