2c269619d13374fc700115964b8e1198b90f3b3e
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2022-12-13 14:25:52 +0100

    Add inner track to track factory

--- src/levels/level1_factory.cpp
+++ src/levels/level1_factory.cpp
@@ -7,6 +7,7 @@ std::shared_ptr<Scene> Level1Factory::get() {
 
     scene->gameobjects
             .push_back(TrackFactory::get("level1-track", "level1", "../assets/tracks/track1.png",
+                                         "../assets/colliders/track1/track1_inner.xml",
                                          "../assets/colliders/track1/track1_outer.xml", scene));
 
     scene->gameobjects.push_back(CarFactory::playerCar("player-car", "car", "../assets/cars/red_car.png", scene));
--- src/track_factory.cpp
+++ src/track_factory.cpp
@@ -6,18 +6,22 @@
 #include <color.hpp>
 
 std::shared_ptr<GameObject>
-TrackFactory::get(const std::string &name, const std::string &tag, std::string sprite_path,  std::string collider_path,
-                  const std::shared_ptr<Scene> &scene) {
+TrackFactory::get(const std::string &name, const std::string &tag, std::string sprite_path,
+                  std::string track_inner_path, std::string track_outer_path, const std::shared_ptr<Scene> &scene) {
     std::shared_ptr<GameObject> obj = std::make_shared<GameObject>(name, tag);
     std::shared_ptr<Sprite> sprite =
             std::make_shared<Sprite>(std::move(sprite_path), Color(0, 0, 0, 0), false, true, 1, 1, 6.f);
     obj->add_component(sprite);
 
-    auto chain_collider = std::make_shared<ChainCollider>(collider_path);
-    auto chain_rigidbody = std::make_shared<RigidBody>(*scene, 6, BodyType::static_body, Vector2d{ 0.f, 0.f}, 1.0f);
-    chain_rigidbody->set_collider(chain_collider);
-    obj->transform.set_angle(degrees_to_radians(180));
-    obj->add_component(chain_rigidbody);
+    auto inner_collider = std::make_shared<ChainCollider>(track_inner_path, false, ColliderNormal::inwards);
+    auto inner_rigidbody = std::make_shared<RigidBody>(*scene, 6, BodyType::static_body, Vector2d{0.f, 0.f}, 1.0f);
+    inner_rigidbody->set_collider(inner_collider);
+    obj->add_component(inner_rigidbody);
+
+    auto outer_collider = std::make_shared<ChainCollider>(track_outer_path);
+    auto outer_rigidbody = std::make_shared<RigidBody>(*scene, 6, BodyType::static_body, Vector2d{0.f, 0.f}, 1.0f);
+    outer_rigidbody->set_collider(outer_collider);
+    obj->add_component(outer_rigidbody);
 
     return obj;
 }
