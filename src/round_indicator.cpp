79ec493506004820e551a5a250ede82d7499e442
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2022-12-15 00:42:46 +0100

    Disable text objects

--- src/round_indicator.cpp
+++ src/round_indicator.cpp
@@ -6,8 +6,8 @@
 GameObject RoundIndicator::get(const std::string& name, const std::string& tag, Car& playerCar) {
     GameObject obj(name, tag);
 
-    std::shared_ptr<Text> text = std::make_shared<Text>(name, tag, 10, 10, std::to_string(playerCar.get_current_round()), "Calibri", 10, Alignment::CENTER, Color(0,0,0,0));
-    obj.add_child(text);
+//    std::shared_ptr<Text> text = std::make_shared<Text>(name, tag, 10, 10, std::to_string(playerCar.get_current_round()), "Calibri", 10, Alignment::CENTER, Color(0,0,0,0));
+//    obj.add_child(text);
 
     return obj;
 }
--- src/speed_indicator.cpp
+++ src/speed_indicator.cpp
@@ -5,8 +5,8 @@
 GameObject SpeedIndicator::get(const std::string &name, const std::string &tag, Car &playerCar) {
     GameObject obj(name, tag);
 
-    std::shared_ptr<Text> text = std::make_shared<Text>(name, tag, 10, 10, std::to_string(playerCar.get_current_speed()), "Calibri", 10, Alignment::CENTER, Color(0,0,0,0));
-    obj.add_child(text);
+//    std::shared_ptr<Text> text = std::make_shared<Text>(name, tag, 10, 10, std::to_string(playerCar.get_current_speed()), "Calibri", 10, Alignment::CENTER, Color(0,0,0,0));
+//    obj.add_child(text);
 
     return obj;
 }
