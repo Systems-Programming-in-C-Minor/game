b1ee19a1e30a481c0ead01b4f7dd1444fc13eeaa
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2022-12-20 19:15:27 +0100

    Add scene switch

--- src/listeners/mode_listener.cpp
+++ src/listeners/mode_listener.cpp
@@ -1,29 +1,35 @@
 #include "listeners/mode_listener.hpp"
 #include "uiobject.hpp"
-#include <iostream>
+#include "global.hpp"
+#include "levels/level1_factory.hpp"
+#include "modes/singleplayer_mode.hpp"
+#include "modes/coop_mode.hpp"
+#include "modes/controller_mode.hpp"
 
-ModeListener::ModeListener(EventManager &event_manager) : GameObject("mode-listener", "mode-listener"), UiObjectListener(event_manager) {}
+ModeListener::ModeListener(EventManager &event_manager) : GameObject("mode-listener", "mode-listener"),
+                                                          UiObjectListener(event_manager) {}
 
 void ModeListener::on_uiobject_released(const UiObjectReleasedEvent &event) {
     if (event.button != BUTTON_LEFT)
         return;
 
     const auto name = event.ui_object.get_name();
+    const auto level = Level1Factory::get();
 
     if (name == "singleplayer") {
-        std::cout << name << std::endl;
+        Global::get_instance()->get_engine().load_scene(SingleplayerMode::get(level));
         return;
     }
     if (name == "coop") {
-        std::cout << name << std::endl;
+        Global::get_instance()->get_engine().load_scene(CoopMode::get(level));
         return;
     }
     if (name == "controller") {
-        std::cout << name << std::endl;
+        Global::get_instance()->get_engine().load_scene(ControllerMode::get(level));
         return;
     }
     if (name == "multiplayer") {
-        std::cout << name << std::endl;
+        // TODO implement multiplayer mode
         return;
     }
 }
