804bda130b99122c0015596ef314be7e6d9e0209
Laerositus <33722419+Laerositus@users.noreply.github.com>
2022-12-20 23:04:14 +0100

    added background_music

--- src/levels/level2_factory.cpp
+++ src/levels/level2_factory.cpp
@@ -1,6 +1,11 @@
 #include "levels/level2_factory.hpp"
 
+#include "components/audiosource.hpp"
+
 RaceLevel Level2Factory::get() {
     std::shared_ptr<Scene> scene = std::make_shared<Scene>(std::shared_ptr<Camera>());
+
+    const auto background_music = std::make_shared<AudioSource>("./assets/audio/background2.mp3", false, false, 0.1, "background");
+    background_music->play();
     return RaceLevel{{}, {}, scene};
 }
