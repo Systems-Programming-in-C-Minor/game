6032dcb0069eaccfecf8f5f533b980c50b283613
Maik de Kruif <maik@maik.dev>
2022-12-21 03:22:07 +0100

    Start game, enable debug and toggle fullscreen using controller

--- include/behaviours/game_behaviour.hpp
+++ include/behaviours/game_behaviour.hpp
@@ -7,17 +7,33 @@
 #include "listeners/checkpoint_listener.hpp"
 #include "race/objects/car.hpp"
 #include "listeners/multiplayer_listener.hpp"
+#include "listeners/joystick_listener.hpp"
 #include <optional>
 
 class GameBehaviour
-        : public GameObject, protected KeyListener, protected CheckpointListener, protected MultiplayerListener {
+        : public GameObject,
+          protected KeyListener,
+          protected JoystickListener,
+          protected CheckpointListener,
+          protected MultiplayerListener {
 public:
     explicit GameBehaviour(EventManager &event_manager, std::vector<std::shared_ptr<Car>> cars,
                            int number_of_laps = 3, int cars_to_finish = 3);
 
 protected:
     void finish();
+
     void start();
+
+    void toggle_fullscreen();
+
+    void toggle_debug();
+
+    void speedup_game();
+
+    void slowdown_game();
+
+    void on_button_pressed(const JoystickButtonPressedEvent &event) override;
 
     void on_key_pressed(const KeyPressedEvent &event) override;
 
@@ -32,13 +48,14 @@ protected:
     void on_stop_game(const StopGameMultiplayerEvent &event) override;
 
 private:
-    bool _alt_pressed = false;
     bool _started = false;
     bool _finished = false;
     int _number_of_laps;
     int _cars_to_finish;
 
     std::map<std::shared_ptr<Car>, int> _cars;
+
+    void _start();
 };
 
 #endif //GAME_GAME_BEHAVIOUR_HPP
