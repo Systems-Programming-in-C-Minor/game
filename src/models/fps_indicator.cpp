fdffa3bd4b2bc241e8e35d65987df6d09d479be7
LM <33723142+Lord-Mustang@users.noreply.github.com>
2022-12-21 22:59:20 +0100

    Added ability to hide FPS indicator

--- src/models/fps_indicator.cpp
+++ src/models/fps_indicator.cpp
@@ -7,3 +7,27 @@ void FpsIndicator::tick(GameObject &_game_object) {
     const unsigned long fps = Global::get_instance()->get_engine().get_fps();
     text->set_text(std::to_string(fps));
 }
+
+void FpsIndicator::on_key_pressed(const KeyPressedEvent& event)
+{
+    const auto alt_pressed = Global::get_instance()->input.get_key(ALT_LEFT);
+
+    switch (event.key) {
+    case D: {
+        if (alt_pressed) {
+            if(_is_visible){
+				game_object->get_component<Text>()->set_foreground_color(Color{0, 0, 0, 1});
+                game_object->get_component<Text>()->set_background_color(Color{0, 0, 0, 1});
+                _is_visible = !_is_visible;
+            } else {
+				game_object->get_component<Text>()->set_foreground_color(Color{0, 255, 0, 255});
+                game_object->get_component<Text>()->set_background_color(Color{0, 0, 0, 150});
+                _is_visible = !_is_visible;
+            }
+        }
+        break;
+    }
+    default:
+        break;
+    }
+}
