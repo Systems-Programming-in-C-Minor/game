124a9ac6f56b8a475ed879b1ad100d49fd6ae410
LM <33723142+Lord-Mustang@users.noreply.github.com>
2022-12-21 18:28:22 +0100

    Changed BoxCollider of TireStack to CircleCollider

--- src/colliders/tire_stack_factory.cpp
+++ src/colliders/tire_stack_factory.cpp
@@ -1,7 +1,7 @@
 #include "colliders/tire_stack_factory.hpp"
 #include "transform.hpp"
 #include "components/sprite.hpp"
-#include "components/colliders/boxcollider.hpp"
+#include "components/colliders/circlecollider.hpp"
 
 std::shared_ptr<GameObject> TireStackFactory::get(Vector2d pos, const std::shared_ptr<Scene> &scene) {
     auto obj = std::make_shared<GameObject>("tire-stack", "tire-stack");
@@ -9,8 +9,8 @@ std::shared_ptr<GameObject> TireStackFactory::get(Vector2d pos, const std::share
     const auto sprite = std::make_shared<Sprite>("./assets/obstacles/tire_stack.png", 20, 200.f);
     obj->add_component(sprite);
 
-    auto collider = std::make_shared<BoxCollider>(1.4f, 1.4f);
-    auto rigid_body = std::make_shared<RigidBody>(*scene, 20, BodyType::static_body, pos, 1.f);
+    auto collider = std::make_shared<CircleCollider>(0.8f);
+    auto rigid_body = std::make_shared<RigidBody>(*scene, 20, BodyType::static_body, pos);
     rigid_body->set_collider(collider);
     obj->add_component(rigid_body);
 
